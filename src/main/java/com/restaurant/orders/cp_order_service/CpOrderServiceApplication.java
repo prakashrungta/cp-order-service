f1f11047cdcc160a9a3d94cd7e07a518419e2b76
D`Souza, Cedric (Cedric) <cedric10101980@gmail.com>
2025-03-12 18:19:46 +0530

    Initial Commit

---
+++ src/main/java/com/restaurant/orders/cp_order_service/CpOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.orders.cp_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/orders/cp_order_service/CpOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.orders.cp_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
