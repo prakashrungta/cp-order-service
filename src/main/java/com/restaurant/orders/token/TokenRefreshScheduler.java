6de939c0cbbdfa9183076f353a3cc07976311792
Prakash Rungta <contact.prakash.rungta@gmail.com>
2025-03-23 00:07:42 +0530

    Updating the Realm exports

--- src/main/java/com/restaurant/orders/token/TokenRefreshScheduler.java
+++ src/main/java/com/restaurant/orders/token/TokenRefreshScheduler.java
@@ -11,9 +11,9 @@ public class TokenRefreshScheduler {
         this.keycloakTokenService = keycloakTokenService;
     }
 
-    @Scheduled(fixedRate = 15 * 10 * 1000 ) // Runs every 10 minutes
+    @Scheduled(fixedRate = 20 * 60 * 1000 ) // Runs every 15 minutes
     public void refreshToken() {
-        System.out.println("Scheduler called  after 10 min for Token Refresh");
+        System.out.println("Scheduler called  after 20 min for Token Refresh");
         keycloakTokenService.refreshServiceAccessToken();
     }
 }
